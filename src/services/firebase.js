462b13365273cc803a9e9318ba5667ba775e5ec6
brunaabc <brandaobrunac@gmail.com>
2025-06-02 20:44:33 -0300

    atualizando

---
+++ src/services/firebase.js
@@ -0,0 +1,21 @@
+import {initializeApp} from "firebase/app";
+import {getAnalytics} from "firebase/analytics";
+
+
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCXdqJnQENLydTGPd77LIrmskTyRUWDY7g",
+  authDomain: "capital-coffee.firebaseapp.com",
+  projectId: "capital-coffee",
+  storageBucket: "capital-coffee.firebasestorage.app",
+  messagingSenderId: "952302457474",
+  appId: "1:952302457474:web:ae47d79ce8d191a838da9b",
+  measurementId: "G-E0C11713X0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
